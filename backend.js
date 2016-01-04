9b791ac5758f86221a5c369f6ac604a363e552f5
Bishaka Samuel <sbishaka@gmail.com>
2016-01-04 18:37:14 +0300

    Close hubisotool/backend#1
    Debounce alert windows with less than 1 second difference

--- backend.js
+++ backend.js
@@ -3,6 +3,21 @@ var
     notifier = require('nw-notify'),
     db = require("./db/db"),
     hubhb = require("hubhb"),
+    moment = require('moment'),
+    debounce = function(func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    },
     backend = module.exports = {};
 
     notifier.setConfig({
@@ -10,13 +25,12 @@ var
     })
 ;
 
-backend.alert = function(opts){
+backend.alert = debounce(function(opts){
     return new Promise(function(alrt_resolve, alrt_reject){
-
         var
             in_notify = {
-            title : opts.title || "",
-            text : opts.text || opts.txt || opts.message || opts.msg || ""
+                title : opts.title || "",
+                text : opts.text || opts.txt || opts.message || opts.msg || ""
             },
 
             in_db = {
@@ -24,13 +38,34 @@ backend.alert = function(opts){
                 title : opts.title || "",
                 msg : opts.text || opts.txt || opts.message || opts.msg || ""
             }
-        ;
+            ;
+        db.execInDb("alerts","find",[ { $and: [{ src: in_db.src }, { title: in_db.title }, {msg: in_db.msg}] } ]).then(function(alerts){
+            if(alerts.length==0){
+                notifier.notify(in_notify);
+                db.addAlert(in_db);
+                return;
+            }
 
-        notifier.notify(in_notify);
-        db.addAlert(in_db);
+            alerts.sort(function(a,b){
+                return moment(a["_timestamp"]).isAfter(b["_timestamp"]);
+            });
+
+            var alert = alerts[alerts.length-1],
+                tt_check = moment().subtract(1,'s');
+
+            if(tt_check.isBefore(alert._timestamp)){
+
+            }else{
+                notifier.notify(in_notify);
+                db.addAlert(in_db);
+            }
+
+        });
+
         alrt_resolve();
     })
-};
+}
+,1000,true);
 
 backend.startup = function(opts){
     db.setDataPath(opts["datapath"]);
